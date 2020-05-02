0b65c85bb2ac9c3c34ba8cd272ad7fc4f568c90b
Azhar Ali <azharalifauzi@gmail.com>
2020-05-02 15:21:56 +0700

    create header component and put to arrange landingpage

---
+++ src/parts/iconText.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Button from "elements/button/button";
+
+const IconText = () => {
+  return (
+    <Button className="brand-text-icon" href="" type="link">
+      Stay<span className="text-gray-900">cation.</span>
+    </Button>
+  );
+};
+
+export default IconText;
